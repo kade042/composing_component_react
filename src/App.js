13897c0d6847daec47a63d824bc6428c0392e135
kade042 <dereje88@gmail.com>
2017-06-06 21:50:07 -0600

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+
+
+class Hello extends Component {
+  render() {
+    return (
+      <h1>Hello World!!</h1>
+    );
+  }
+}
+
+
+React.render(<Hello />, document.getElementById('root'));
